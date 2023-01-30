3a58f2992aa6d5beb5b0bcb45403950a7841cd60
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2023-01-30 18:00:11 +0200

    Add compile script.

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+mkdir -p build/$1
+# cmake -S$1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -H. -Bbuild/$1
+cmake -S$1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -Bbuild/$1
+cd build/$1
+make -j4 all
+cd ../..
